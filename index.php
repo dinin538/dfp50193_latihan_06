bbacb8df25efaf8beea45f80aaaaee15a7fc0986
dinin538 <dnnblh@gmail.com>
2021-10-07 09:47:55 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan</title>
+</head>
+<body>
+    <?php
+    $z=0;
+    $x=17;
+    for ($y=50; $y>$x; $y -= $z) {
+        $z++;
+        
+    }
+    echo "$z helai baju, baki duit = $y";
+    ?>
+</body>
+</html>
